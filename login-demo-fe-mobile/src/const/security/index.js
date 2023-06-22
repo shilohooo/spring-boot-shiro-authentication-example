6e51b22d7d6bf7804497b580a8408b3da5c4830a
shiloh <lixiaolei595@gmail.com>
2023-06-22 14:15:27 +0800

    refactor: add constants

---
+++ login-demo-fe-mobile/src/const/security/index.js
@@ -0,0 +1,12 @@
+/**
+ * 安全相关常量
+ * @author shiloh
+ * @date 2023/6/22 12:11
+ */
+
+/**
+ * token cookie 名称
+ * @author shiloh
+ * @date 2023/6/22 12:12
+ */
+export const TOKEN_COOKIE_KEY = 'token'
---
+++ login-demo-fe-mobile/src/const/user/index.js
@@ -0,0 +1,12 @@
+/**
+ * 用户相关的常量
+ * @author shiloh
+ * @date 2023/6/22 12:12
+ */
+
+/**
+ * 会话存储中用户信息的 key 名称
+ * @author shiloh
+ * @date 2023/6/22 12:12
+ */
+export const USER_SESSION_KEY = 'USER_INFO'
